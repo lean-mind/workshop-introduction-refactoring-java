8685baca97f444100bc318c295c67a699949dcde
Cristian Suárez Vera <cristian.suarez-ext@clarity.ai>
2023-11-20 21:47:04 +0000

    exercise 2.1: remove old implementation.
    In this point, we have finished the parallel change.

--- src/main/java/signatureChange/AuthenticationService.java
+++ src/main/java/signatureChange/AuthenticationService.java
@@ -2,12 +2,7 @@ package signatureChange;
 
 public class AuthenticationService {
 
-    private int adminId = 12345;
     private ID adminId2 = new ID(12345);
-
-    public boolean isAuthenticated(int id) {
-        return id == adminId;
-    }
 
     public boolean isAuthenticated(ID id) {
         return id.equals(adminId2);
