a04c711b6ff4945f0d43e44788f724507322f9d2
Cristian Suárez Vera <cristian.suarez-ext@clarity.ai>
2023-11-20 20:41:59 +0000

    exercise 1.2: remove old Integer list of prices

--- src/main/java/implementationChange/ShoppingCart.java
+++ src/main/java/implementationChange/ShoppingCart.java
@@ -4,16 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ShoppingCart {
-    private List<Integer> prices;
     private List<Price> prices2;
 
     public ShoppingCart() {
-        prices = new ArrayList<>();
         prices2 = new ArrayList<>();
     }
 
     public void add(int price) {
-        this.prices.add(price);
         this.prices2.add(new Price(price));
     }
 
