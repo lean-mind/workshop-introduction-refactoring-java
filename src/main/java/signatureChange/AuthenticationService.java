f8f6cd05cd2470e1516d62b26850942a3aced7c0
Cristian Suárez Vera <cristian.suarez-ext@clarity.ai>
2023-11-20 21:45:18 +0000

    exercise 2.1: generate hasCode and equals to implement new authentication

--- src/main/java/signatureChange/AuthenticationService.java
+++ src/main/java/signatureChange/AuthenticationService.java
@@ -1,11 +1,15 @@
 package signatureChange;
 
 public class AuthenticationService {
+
+    private int adminId = 12345;
+    private ID adminId2 = new ID(12345);
+
     public boolean isAuthenticated(int id) {
-        return id == 12345;
+        return id == adminId;
     }
 
     public boolean isAuthenticated(ID id) {
-        return false;
+        return id.equals(adminId2);
     }
 }
--- src/main/java/signatureChange/ID.java
+++ src/main/java/signatureChange/ID.java
@@ -1,9 +1,25 @@
 package signatureChange;
 
+import java.util.Objects;
+
 public class ID {
     private final int value;
 
     public ID(int value) {
         this.value = value;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ID id = (ID) o;
+        return value == id.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
 }
