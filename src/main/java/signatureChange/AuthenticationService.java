03e1bb7c177552c0378a33fca86a33cca0318918
Cristian Suárez Vera <cristian.suarez-ext@clarity.ai>
2023-11-20 21:55:54 +0000

    exercise 2.2: remove duplicated tests

--- src/main/java/signatureChange/AuthenticationService.java
+++ src/main/java/signatureChange/AuthenticationService.java
@@ -2,10 +2,9 @@ package signatureChange;
 
 public class AuthenticationService {
 
-    private int adminID = 12345;
-    private ID adminID2 = new ID(12345);
+    private ID adminID = new ID(12345);
 
     public boolean isAuthenticated(ID id) {
-        return id.equals(adminID2);
+        return id.equals(adminID);
     }
 }
--- src/test/java/unit/AuthenticationServiceShould.java
+++ src/test/java/unit/AuthenticationServiceShould.java
@@ -10,24 +10,11 @@ public class AuthenticationServiceShould {
     @Test
     public void distinguish_administrator_role() throws Exception {
         AuthenticationService service = new AuthenticationService();
-        int adminId = 12345;
-        Assert.assertTrue(service.isAuthenticated(new ID(adminId)));
-    }
-    @Test
-    public void distinguish_non_admin_role() throws Exception {
-        AuthenticationService service = new AuthenticationService();
-        int normalUserId = 11111;
-        Assert.assertFalse(service.isAuthenticated(new ID(normalUserId)));
-    }
-
-    @Test
-    public void distinguish_administrator_role_value_object() throws Exception {
-        AuthenticationService service = new AuthenticationService();
         ID adminId = new ID(12345);
         Assert.assertTrue(service.isAuthenticated(adminId));
     }
     @Test
-    public void distinguish_non_admin_role_value_object() throws Exception {
+    public void distinguish_non_admin_role() throws Exception {
         AuthenticationService service = new AuthenticationService();
         ID normalUserId = new ID(11111);
         Assert.assertFalse(service.isAuthenticated(normalUserId));
