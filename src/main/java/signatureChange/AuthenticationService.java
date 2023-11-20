1162f2342c361553cdbcadd2ae0dbf65ff5421b6
Cristian Suárez Vera <cristian.suarez-ext@clarity.ai>
2023-11-20 21:55:19 +0000

    exercise 2.2: inline the old implementation.
    Put the caret in the name of the old implementation `isAuthenticated(int id)` and do an inline for this method. Now, all the old implementations will be replaced with the code that had inside of it. In this case, the new implementation that we want to use.
    
    Doing that, we will not need to go one by one replacing all the usages.

--- src/main/java/signatureChange/AuthenticationService.java
+++ src/main/java/signatureChange/AuthenticationService.java
@@ -5,10 +5,6 @@ public class AuthenticationService {
     private int adminID = 12345;
     private ID adminID2 = new ID(12345);
 
-    public boolean isAuthenticated(int id) {
-        return isAuthenticated(new ID(id));
-    }
-
     public boolean isAuthenticated(ID id) {
         return id.equals(adminID2);
     }
--- src/test/java/unit/AuthenticationServiceShould.java
+++ src/test/java/unit/AuthenticationServiceShould.java
@@ -11,13 +11,13 @@ public class AuthenticationServiceShould {
     public void distinguish_administrator_role() throws Exception {
         AuthenticationService service = new AuthenticationService();
         int adminId = 12345;
-        Assert.assertTrue(service.isAuthenticated(adminId));
+        Assert.assertTrue(service.isAuthenticated(new ID(adminId)));
     }
     @Test
     public void distinguish_non_admin_role() throws Exception {
         AuthenticationService service = new AuthenticationService();
         int normalUserId = 11111;
-        Assert.assertFalse(service.isAuthenticated(normalUserId));
+        Assert.assertFalse(service.isAuthenticated(new ID(normalUserId)));
     }
 
     @Test
