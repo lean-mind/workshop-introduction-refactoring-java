18eb30b249c59b88756015afe6c3f7f86b2cde3a
Cristian Suárez Vera <cristian.suarez-ext@clarity.ai>
2023-11-20 21:51:55 +0000

    exercise 2.2: set up parallel change and tests

--- src/main/java/signatureChange/AuthenticationService.java
+++ src/main/java/signatureChange/AuthenticationService.java
@@ -2,7 +2,14 @@ package signatureChange;
 
 public class AuthenticationService {
 
+    private int adminID = 12345;
+    private ID adminID2 = new ID(12345);
+
     public boolean isAuthenticated(int id) {
-        return id == 12345;
+        return id == adminID;
+    }
+
+    public boolean isAuthenticated(ID id) {
+        return id.equals(adminID2);
     }
 }
